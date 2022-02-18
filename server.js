c0c0ce627c7a87c276b6f656fa35bf6e689b880f
David Ku <hola@davidku.dev>
2022-02-17 20:23:19 -0500

    se agrega city a las rutas.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 
 const actorRoutes = require("./routes/v1/actorRoutes");
 const countryRoutes = require("./routes/v1/countryRoutes");
+const cityRoutes = require("./routes/v1/cityRouters");
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 // Rutas
 app.use("/api/v1/actors", actorRoutes);
 app.use("/api/v1/countries", countryRoutes);
+app.use("/api/v1/cities", cityRoutes);
 
 // Iniciar el servidor
 app.listen(8000, () => {
